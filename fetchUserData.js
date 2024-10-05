3decfe66d2b957168c6db1cbe1fbd9d6ba549652
Harshitha <chharshitha277@gmail.com>
2024-10-05 17:02:13 +0530

    "task 6 backend development completed"

--- fetchUserData.js
+++ fetchUserData.js
@@ -1,1 +1,22 @@
+function fetchUserData() {
+    return new Promise((resolve, reject) => {
+        fetch('https://jsonplaceholder.typicode.com/users/1')
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+                resolve({ name: data.name, email: data.email });
+            })
+            .catch(error => {
+                reject('Error fetching user data');
+            });
+    });
+}
+fetchUserData()
+    .then(data => console.log(data))       
+    .catch(error => console.log(error));
+
 
